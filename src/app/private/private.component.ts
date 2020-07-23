eeb9d92bdc021071bc49b3cd9fdeebbe97cd4c13
Daniel Rodera <daniel.rodera@atos.net>
2020-07-23 16:51:51 +0200

    private v1

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  templateUrl: './private.component.html'
-})
+  templateUrl: './private.component.html',
+  styleUrls: ['./private.component.css']})
 export class PrivateComponent implements OnInit {
 
   constructor() { }
